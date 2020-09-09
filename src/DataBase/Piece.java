272c6f784099967d7675a7489d9615b7412bddd1
Carlos Soto <55759561+csotogd@users.noreply.github.com>
2020-09-09 11:54:38 +0200

    Brief piece description

---
+++ src/DataBase/Piece.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.util.List;
+
+public class Piece {
+    private int[][] shape;
+    private boolean used;
+    private List<int[][]> permutations;
+
+    public Piece (int[][]  array){
+      //  this.shape =   should make copy of aray and not array itself
+    }
+
+    //public int[][]getShape(){}
+    public void setShape(){}
+
+    public void rotateLeft(){}
+    public void rotateRight(){}
+    public void rotateUpsideDown(){}
+}
