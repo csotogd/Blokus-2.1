febeaa07d3ce56bfa37096ae5446965e1dc88f79
Carlos <c.sotogd@gmail.com>
2020-09-29 16:31:56 +0200

    getcorner methods into board class

--- src/GameBoard/Board.java
+++ src/GameBoard/Board.java
@@ -12,6 +12,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+
 //TEST
 public class Board extends Pane {
 
@@ -133,6 +135,130 @@ public class Board extends Pane {
         return false;
     }
 
+
+    /**
+     * used to be STATIC METHOD dedined in Corner, where the board was passed as a parameter,
+     * Get all corners on the board of the player with starting position startingPosition
+     * @param startingPosition starting position of the player
+     * @return
+     */
+    public  ArrayList<Corner> getCorner( Vector2d startingPosition){
+    Board board=this;
+        ArrayList<Corner> corners = new ArrayList<>();
+        if(board.board[startingPosition.get_y()][startingPosition.get_x()]==0){
+            Vector2d adjacent=startingPosition.add(new Vector2d(1,1));
+            if(board.inBoard(adjacent)) {
+                corners.add(new Corner(startingPosition,adjacent));
+                return corners;
+            }
+            adjacent=startingPosition.add(new Vector2d(-1,-1));
+            if(board.inBoard(adjacent)) {
+                corners.add(new Corner(startingPosition,adjacent));
+                return corners;
+            }
+            adjacent=startingPosition.add(new Vector2d(1,-1));
+            if(board.inBoard(adjacent)) {
+                corners.add(new Corner(startingPosition,adjacent));
+                return corners;
+            }
+            adjacent=startingPosition.add(new Vector2d(-1,1));
+            if(board.inBoard(adjacent)) {
+                corners.add(new Corner(startingPosition,adjacent));
+                return corners;
+            }
+        }
+        boolean[][] checked = new boolean[board.getDIMENSION().get_y()][board.getDIMENSION().get_x()];
+        findCorners( checked,startingPosition,corners,board.board[startingPosition.get_y()][startingPosition.get_x()]);
+
+        return corners;
+    }
+
+
+
+
+    /**
+     * helper method to find corners and add them to the arraylist
+     * @param checked position on the board that is already checked
+     * @param position current coordinates
+     * @param corners arraylist that contains the corners
+     * @param player id of the player
+     */
+//TODO : not use inboard function as it takes twice as many evaluations as necessary
+    private void findCorners(boolean[][] checked, Vector2d position, ArrayList<Corner> corners, int player) {
+        Board board =this;
+        if(board.inBoard(position) && !checked[position.get_y()][position.get_x()]){
+            checked[position.get_y()][position.get_x()]=true;
+            if(board.board[position.get_y()][position.get_x()]==player){
+                boolean top=true, down=true, left=true, right=true; //NOT occupied by own blocks ie"free"
+                if(board.inBoard(position.add(new Vector2d(0,-1))) && board.board[position.get_y()-1][position.get_x()]==player){
+                    top=false;
+                    findCorners( checked, position.add(new Vector2d(0,-1)), corners, player);
+                }
+                if(board.inBoard(position.add(new Vector2d(0,1))) && board.board[position.get_y()+1][position.get_x()]==player){
+                    down=false;
+                    findCorners( checked, position.add(new Vector2d(0,1)), corners, player);
+                }
+                if(board.inBoard(position.add(new Vector2d(-1,0))) && board.board[position.get_y()][position.get_x()-1]==player){
+                    left=false;
+                    findCorners( checked, position.add(new Vector2d(-1,0)), corners, player);
+                }
+                if(board.inBoard(position.add(new Vector2d(1,0))) && board.board[position.get_y()][position.get_x()+1]==player){
+                    right=false;
+                    findCorners( checked, position.add(new Vector2d(1,0)), corners, player);
+                }
+
+                Corner current=null;//current corner being evaluated
+
+                if(top && left
+                        && board.inBoard(new Vector2d(position.get_x()-1, position.get_y()-1))
+                        && board.board[position.get_y()-1][position.get_x()-1]==0){
+                    current = new  Corner(position,position.add(new Vector2d(-1,-1)));
+                    corners.add(current);
+                }
+                if(top && right
+                        && board.inBoard(new Vector2d(position.get_x()+1, position.get_y()-1))
+                        && board.board[position.get_y()-1][position.get_x()+1]==0){
+                    if(current==null) {
+                        current = new Corner(position,position.add(new Vector2d(1,-1)));
+                        corners.add(current);
+                    }
+                    else {
+                        current.addAdjacent(position.add(new Vector2d(1,-1)));
+                    }
+                }
+                if(down && left
+                        && board.inBoard(new Vector2d(position.get_x()-1, position.get_y()+1))
+                        && board.board[position.get_y()+1][position.get_x()-1]==0){
+                    if(current==null) {
+                        current = new Corner(position,position.add(new Vector2d(-1,1)));
+                        corners.add(current);
+                    }
+                    else {
+                        current.addAdjacent(position.add(new Vector2d(-1,1)));
+                    }
+                }
+                if(down && right
+                        && board.inBoard(new Vector2d(position.get_x()+1, position.get_y()+1))
+                        && board.board[position.get_y()+1][position.get_x()+1]==0){
+                    if(current==null) {
+                        current = new Corner(position,position.add(new Vector2d(1,1)));
+                        corners.add(current);
+                    }
+                    else {
+                        current.addAdjacent(position.add(new Vector2d(1,1)));
+                    }
+                }
+
+                //now check for corners
+                if(board.isOccupiedBy(position.add(new Vector2d(1,1)),player)) findCorners( checked,position.add(new Vector2d(1,1)),corners,player);
+                if(board.isOccupiedBy(position.add(new Vector2d(-1,1)),player)) findCorners(checked,position.add(new Vector2d(-1,1)),corners,player);
+                if(board.isOccupiedBy(position.add(new Vector2d(-1,-1)),player)) findCorners(checked,position.add(new Vector2d(-1,-1)),corners,player);
+                if(board.isOccupiedBy(position.add(new Vector2d(1,-1)),player)) findCorners(checked,position.add(new Vector2d(1,-1)),corners,player);
+
+            }
+
+        }
+    }
 
     public int[][] getBoard() {
         return board;
