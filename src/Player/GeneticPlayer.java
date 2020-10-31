e754aa03f4b75cbcea48861393296b32dee7f61e
Carlos <c.sotogd@gmail.com>
2020-10-31 11:28:32 +0100

    structure making sense
    probably the class structure to work with

--- src/Player/GeneticPlayer.java
+++ src/Player/GeneticPlayer.java
@@ -8,7 +8,7 @@ import Tools.Vector2d;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Map;
 
 
 public class GeneticPlayer extends BotPlayer {
@@ -25,10 +25,19 @@ public class GeneticPlayer extends BotPlayer {
        //while we code all the different strategies, make this call the one you want to try
         System.out.println("In calculate move for genetic algorithm");
         HashMap<Move, Float> movesAndScores = new HashMap<Move, Float>();
+        //this map will store all the moves and their respective score,
+        // scores will be modified throuout the algorithm.
+        //see example in far from starting corner
         for (Move move : super.possibleMoveSet(board)){
             movesAndScores.put(move, new Float(0));
         }
-
+        
+        //the weights is what we will calculate in the genetic algorithm
+        addsMostCorners(1,movesAndScores, board);
+        blocksMostCorners(1,movesAndScores, board);
+        closestToMiddle(1,movesAndScores, board);
+        biggestPiece(1,movesAndScores, board);
+        farFromStartingCorner(1,movesAndScores, board);
 
     return null;
     }
@@ -43,9 +52,42 @@ public class GeneticPlayer extends BotPlayer {
     private void biggestPiece(float weight, HashMap<Move, Float> movesAndScores,Board board){;}
 
     //TODO: test it
-    //Dont try to imitate what I did here, as it is wrong and not finished
+
+    /**
+     * strategy that calculates how far away from the starting corner a move is
+     *
+     * @param weight
+     * @param movesAndScores
+     * @param board
+     */
     private void farFromStartingCorner(float weight, HashMap<Move, Float> movesAndScores,Board board){
 
+        for (Map.Entry<Move, Float> entry : movesAndScores.entrySet()) {
+            //for every move, see which of the corner of the piece is furthest away from the starting corner.
+            //score will be the distance between it and the corner. The further away the greater the distance
+            Move move=entry.getKey();
+            ArrayList<Corner> cornerContacts=move.getPiece().getCornersContacts(move.getPosition());
+            int maxDistance=-1;
+            for(Corner corner: cornerContacts ){
+                Vector2d cornerPosition=corner.getPosition();
+                int distance= cornerPosition.moduleDistance(this.startingCorner);
+                if(distance>maxDistance){
+                    maxDistance=distance;
+                }
+
+            }
+
+            //the score if weight was one needs to be between [0, 1]
+            float normalization= (float) Math.sqrt(2*(board.getDIMENSION()^2));
+            float score = weight*(maxDistance/normalization);
+
+            //now the  for each move is updated according to what we calculated and the weight given.
+            movesAndScores.put(move, movesAndScores.get(move) + score);
+
+        }
+
+        //commented alternative IGNORE THIS!!!
+/*
         ArrayList<Move> possibleMoves = super.possibleMoveSet(board);
         for (Move move: possibleMoves){
             //for every move, see which of the corner of the piece is furthest awy from the starting corner.
@@ -64,6 +106,9 @@ public class GeneticPlayer extends BotPlayer {
             //TODO assign score to move....discuss how that will be done
 
             }
+
+*/
+
 
 
 
