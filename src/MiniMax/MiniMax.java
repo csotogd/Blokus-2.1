63f84283f7230b628c3ba8913b5d8e75811fd99e
martin <martinmichaux2001@gmail.com>
2020-11-21 11:17:54 +0100

    test on "killer moves" boost

--- src/MiniMax/MiniMax.java
+++ src/MiniMax/MiniMax.java
@@ -7,12 +7,17 @@ import Move.Move;
 import Tools.Vector2d;
 import Player.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MiniMax {
     Player[] players;
     Board board;
     private final int NBR_OF_TURNS = 1;
     private int maxDepth;
     int rootPlayerNbr;
+    HashMap<Integer,Move> cutOffMoveOccurence;
 
     public MiniMax(Player[] players, Board board){
         this.players = players;
@@ -20,7 +25,6 @@ public class MiniMax {
         for(Player player:players){
             player.reOrderPieceList();
         }
-
          */
         this.board = board;
         maxDepth = players.length*NBR_OF_TURNS;
@@ -28,6 +32,7 @@ public class MiniMax {
 
     public Move getMove(int playerNbr){
         this.rootPlayerNbr = playerNbr;
+        this.cutOffMoveOccurence = new HashMap<>();
         MiniMaxNode root = new MiniMaxNode(board,players[playerNbr-1]);
         //create first nodes of that player
         return alphaBeta_Pruning(root,root.getDepth(),playerNbr,Float.MIN_VALUE,Float.MAX_VALUE).getMove();
@@ -41,6 +46,7 @@ public class MiniMax {
         }else{
             //TODO order the move so that the worst ones are at the end and are pruned faster
             for (Move possibleMove : players[playerNbr-1].possibleMoveSetBoosted(board)){
+                //TODO compute first the moves that have been cutoff earlier
                 node = new MiniMaxNode(node,possibleMove,depth+1,players[playerNbr-1]);
                 int nextPlayerNbr = 0;
                 if(playerNbr>=players.length){
@@ -54,13 +60,57 @@ public class MiniMax {
                     alpha = Math.max(alpha, alphaBeta_Pruning(node.getParent(),depth+1,nextPlayerNbr,alpha,beta).getScore());
                 }
                 if(alpha>=beta){
+                    //cutoff
+                    checkToAddToCutOff(node.getMove());
                     node.setScore(beta);
                     return node;
                 }
             }
+            updateOcc();
             node.setScore(alpha);
             return node;
         }
+    }
+
+    public void updateOcc(){
+        for(Map.Entry<Integer, Move> entry : cutOffMoveOccurence.entrySet()){
+            int oldOcc = entry.getKey();
+            Move oldMove = entry.getValue();
+            cutOffMoveOccurence.remove(entry);
+            cutOffMoveOccurence.put(oldOcc-1,oldMove);
+        }
+    }
+
+    public void checkToAddToCutOff(Move move){
+        if(cutOffMoveOccurence.isEmpty()){
+            cutOffMoveOccurence.put(1,move);
+        }else{
+            if(cutOffMoveOccurence.containsValue(move)){
+                increaseOcc(move);
+            }else{
+                replaceSmallestOcc(move);
+            }
+        }
+    }
+
+    public void increaseOcc(Move move){
+        int previousOcc = 0;
+        for(Map.Entry<Integer, Move> entry : cutOffMoveOccurence.entrySet()){
+            if(entry.getValue().equals(move)){
+                previousOcc = entry.getKey();
+                cutOffMoveOccurence.remove(entry);
+            }
+        }
+        cutOffMoveOccurence.put(++previousOcc,move);
+    }
+
+    public void replaceSmallestOcc(Move move){
+        int min = Integer.MAX_VALUE;
+        for(Integer i:cutOffMoveOccurence.keySet()){
+            if(i<min)min = i;
+        }
+        cutOffMoveOccurence.remove(min);
+        cutOffMoveOccurence.put(1,move);
     }
 
     public static void main(String[] args) {
@@ -82,18 +132,22 @@ public class MiniMax {
         int i= 0;
         while(i<15){
             Move move1 = m.getMove(p1.getPlayerNumber());
+            System.out.println(Arrays.toString(m.cutOffMoveOccurence.keySet().toArray()));
             if(move1.makeMove(b)) p1.removePiece(move1.getPiece().getLabel());
             b.print();
 
             Move move2 = m.getMove(p2.getPlayerNumber());
+            System.out.println(Arrays.toString(m.cutOffMoveOccurence.keySet().toArray()));
             if(move2.makeMove(b)) p2.removePiece(move1.getPiece().getLabel());
             b.print();
 
             Move move3 = m.getMove(p3.getPlayerNumber());
+            System.out.println(Arrays.toString(m.cutOffMoveOccurence.keySet().toArray()));
             if(move3.makeMove(b)) p3.removePiece(move1.getPiece().getLabel());
             b.print();
 
             Move move4 = m.getMove(p4.getPlayerNumber());
+            System.out.println(Arrays.toString(m.cutOffMoveOccurence.keySet().toArray()));
             if(move4.makeMove(b)) p4.removePiece(move1.getPiece().getLabel());
             b.print();
 
